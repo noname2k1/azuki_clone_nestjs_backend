b105bfe2a95bfa42e14a735d49cea19ae6e5d2a8
noname2k1 <54305191+noname2k1@users.noreply.github.com>
2023-06-04 13:25:30 +0700

    fix-bug

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,7 @@
 import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UserSchema } from '@/schemas/user.schema';
-import { Bcrypt } from '@/utils/Bcrypt';
+import { UserSchema } from '@/schemas/User.schema';
 
 @Module({
   imports: [
